44a3850735afa34d7c8bf2c7d4b651977122d819
Rajab-ali99 <rajabli2649gmail.com>
2025-12-14 23:35:28 +0500

    google pixel added

--- app/layout.jsx
+++ app/layout.jsx
@@ -54,7 +54,21 @@ export default function RootLayout({ children }) {
     <html className="dark" lang="en">
       <head>
 
-        
+          {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-912P3ZMTB5"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-912P3ZMTB5');
+          `}
+        </Script>
+        {/* End Google tag */}
 
 
         <script
