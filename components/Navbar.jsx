58c5c1f9e866194d7299b9ab1206d4ebe3ff67b5
Rajab-ali99 <rajabli2649gmail.com>
2026-05-16 16:59:03 +0500

    logo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
       <Link href={WEBSITE_HOME}>
       <div className=' md:px-16 px-2 '>
         <Image className='w-30 hidden md:block h-auto' src={logoD.src} height={logoD.height} width={logoD.width} alt='logo'/>
-        <Image className='w-18  md:hidden h-auto' src={logoM.src} height={logoM.height} width={logoM.width} alt='logo'/>
+        <Image className='w-16  md:hidden h-auto' src={logoM.src} height={logoM.height} width={logoM.width} alt='logo'/>
       </div>
       </Link>
       <div className='bg-black py-2 hidden  font-bold text-lg px-4 border  border-slate-700 rounded-full text-white md:flex items-center gap-5 '>
@@ -33,7 +33,7 @@ const Navbar = () => {
 
 
       </div>
-      <div className="text-2xl font-bold  text-orange-500 md:hidden">
+      <div className="text-3xl font-bold  text-orange-500 md:hidden">
         Bullish <span className="text-white">Pixel</span> 
       </div>
       <div className='hidden md:block'>
